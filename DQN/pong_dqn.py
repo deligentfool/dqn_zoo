a48825c59fc6a876b195b8b3c80e63e2511a05a5
xzw <1027660817@qq.com>
2020-02-10 14:59:18 +0800

    :construction: add atari env

--- DQN/pong_dqn.py
+++ DQN/pong_dqn.py
@@ -1,34 +1,15 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import random
+import numpy as np
 from collections import deque
+import random
 from common.wrappers import make_atari, wrap_deepmind, wrap_pytorch
 import math
 
-class replay_buffer(object):
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = deque(maxlen=self.capacity)
-
-    def store(self, observation, action, reward, next_observation, done):
-        observation = np.expand_dims(observation, 0)
-        next_observation = np.expand_dims(next_observation, 0)
-        self.memory.append([observation, action, reward, next_observation, done])
-
-    def sample(self, batch_size):
-        batch = random.sample(self.memory, batch_size)
-        observations, actions, rewards, next_observations, dones = zip(* batch)
-        return np.concatenate(observations, 0), actions, rewards, np.concatenate(next_observations, 0), dones
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class cnn_ddqn(nn.Module):
+class cnn_dqn(nn.Module):
     def __init__(self, observation_dim, action_dim):
-        super(cnn_ddqn, self).__init__()
+        super(cnn_dqn, self).__init__()
         self.observation_dim = observation_dim
         self.action_dim = action_dim
 
@@ -58,7 +39,7 @@ class cnn_ddqn(nn.Module):
         x = self.fc2(x)
         return x
 
-    def act(self, observation, epsilon):
+    def get_action(self, observation, epsilon):
         if random.random() > epsilon:
             q_value = self.forward(observation)
             action = q_value.max(1)[1].data[0].item()
@@ -67,7 +48,26 @@ class cnn_ddqn(nn.Module):
         return action
 
 
-def train(buffer, target_model, eval_model, gamma, optimizer, batch_size, loss_fn, count, soft_update_freq):
+class replay_buffer(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = deque(maxlen=self.capacity)
+
+    def store(self, observation, action, reward, next_observation, done, ):
+        observation = np.expand_dims(observation, 0)
+        next_observation = np.expand_dims(next_observation, 0)
+        self.memory.append([observation, action, reward, next_observation, done])
+
+    def sample(self, size):
+        batch = random.sample(self.memory, size)
+        observation, action, reward, next_observation, done = zip(* batch)
+        return np.concatenate(observation, 0), action, reward, np.concatenate(next_observation, 0), done
+
+    def __len__(self):
+        return len(self.memory)
+
+
+def training(buffer, batch_size, model, optimizer, gamma, loss_fn):
     observation, action, reward, next_observation, done = buffer.sample(batch_size)
 
     observation = torch.FloatTensor(observation)
@@ -76,20 +76,19 @@ def train(buffer, target_model, eval_model, gamma, optimizer, batch_size, loss_f
     next_observation = torch.FloatTensor(next_observation)
     done = torch.FloatTensor(done)
 
-    q_values = eval_model.forward(observation)
-    next_q_values = target_model.forward(next_observation)
-    argmax_actions = eval_model.forward(next_observation).max(1)[1].detach()
-    next_q_value = next_q_values.gather(1, argmax_actions.unsqueeze(1)).squeeze(1)
+    q_values = model.forward(observation)
+    next_q_values = model.forward(next_observation)
+
     q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
-    expected_q_value = reward + gamma * (1 - done) * next_q_value
+    next_q_value = next_q_values.max(1)[0].detach()
+    expected_q_value = reward + next_q_value * (1 - done) * gamma
 
     loss = loss_fn(q_value, expected_q_value)
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    if count % soft_update_freq == 0:
-        target_model.load_state_dict(eval_model.state_dict())
+
 
 
 if __name__ == '__main__':
@@ -100,11 +99,11 @@ if __name__ == '__main__':
     capacity = 100000
     exploration = 10000
     batch_size = 32
-    gamma = 0.99
-    learning_rate = 1e-4
-    soft_update_freq = 1000
     episode = 1000000
     render = True
+    learning_rate = 1e-5
+    gamma = 0.99
+    loss_fn = nn.MSELoss()
 
     env_id = "PongNoFrameskip-v4"
     env = make_atari(env_id)
@@ -112,17 +111,13 @@ if __name__ == '__main__':
     env = wrap_pytorch(env)
     action_dim = env.action_space.n
     observation_dim = env.observation_space.shape
-    action_dim = env.action_space.n
-    target_net = cnn_ddqn(observation_dim, action_dim)
-    eval_net = cnn_ddqn(observation_dim, action_dim)
-    eval_net.load_state_dict(target_net.state_dict())
-    optimizer = torch.optim.Adam(eval_net.parameters(), lr=learning_rate)
+
+    model = cnn_dqn(observation_dim, action_dim)
+    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
     buffer = replay_buffer(capacity)
-    loss_fn = nn.MSELoss()
-    epsilon = epsilon_init
+    weight_reward = None
     count = 0
 
-    weight_reward = None
     for i in range(episode):
         obs = env.reset()
         reward_total = 0
@@ -130,22 +125,20 @@ if __name__ == '__main__':
             env.render()
         while True:
             epsilon = epsilon_by_count(count)
-            action = eval_net.act(torch.FloatTensor(np.expand_dims(obs, 0)), epsilon)
-            count += 1
+            action = model.get_action(torch.FloatTensor(np.expand_dims(obs, 0)), epsilon)
             next_obs, reward, done, info = env.step(action)
+            if render:
+                env.render()
             buffer.store(obs, action, reward, next_obs, done)
             reward_total += reward
             obs = next_obs
-            if render:
-                env.render()
+            count += 1
             if len(buffer) > exploration:
-                train(buffer, target_net, eval_net, gamma, optimizer, batch_size, loss_fn, count, soft_update_freq)
-
+                training(buffer, batch_size, model, optimizer, gamma, loss_fn)
             if done:
                 if not weight_reward:
                     weight_reward = reward_total
                 else:
-                    weight_reward = 0.9 * weight_reward + 0.1 * reward_total
-                print('episode: {}  epsilon: {:.2f}  reward: {}  weight_reward: {:.3f}'.format(i+1, epsilon, reward_total, weight_reward))
+                    weight_reward = 0.99 * weight_reward + 0.01 * reward_total
+                print('episode: {}  reward: {}  epsilon: {:.5f}  weight_reward: {:.3f}'.format(i+1, reward_total, epsilon, weight_reward))
                 break
-
